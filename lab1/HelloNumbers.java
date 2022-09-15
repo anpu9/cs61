f193c3f4ab1464fbe5dbd1146b70b1b789162592
Yuyang Tian <yuyangtian@Yuyangs-MacBook-Air.local>
2022-09-15 10:41:34 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,10 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0, res = 0;
+        while (x < 10) {
+            res+=x;
+            System.out.print(res+ " ");
+            x = x + 1;
+        }
+    }
+}
